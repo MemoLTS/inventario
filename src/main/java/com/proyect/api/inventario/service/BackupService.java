33216a9c606251bcdc599bfe69fd5712d9b60e06
MemoLTS <guil.toledo@duocuc.cl>
2026-05-19 13:44:14 -0400

    "first commit: add stock field to Producto and update InvService to handle stock updates"

---
+++ src/main/java/com/proyect/api/inventario/service/BackupService.java
@@ -0,0 +1,62 @@
+package com.proyect.api.inventario.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+@Service
+public class BackupService {
+    private final String DB_NAME = "inventario";
+    private final String DB_USER = "root";
+
+    private final String MYSQL_DUMP_PATH =
+            "C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysqldump.exe";
+
+    private final String BACKUP_PATH =
+            "C:\\backups\\";
+
+    @Scheduled(cron = "0 0 2 * * SUN")
+    public void realizarBackup() {
+    try {
+
+        File directorio = new File(BACKUP_PATH);
+
+        if (!directorio.exists()) {
+            directorio.mkdirs();
+        }
+
+        String fecha = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss")
+                .format(new Date());
+
+        String backupFile =
+                BACKUP_PATH + "backup_" + fecha + ".sql";
+
+        ProcessBuilder processBuilder = new ProcessBuilder(
+                MYSQL_DUMP_PATH,
+                "-u",
+                DB_USER,
+                DB_NAME,
+                "-r",
+                backupFile
+        );
+
+        processBuilder.redirectErrorStream(true);
+
+        Process process = processBuilder.start();
+
+        int resultado = process.waitFor();
+
+        if (resultado == 0) {
+            System.out.println("Backup creado correctamente");
+        } else {
+            System.out.println("Error al crear backup");
+        }
+
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}}
